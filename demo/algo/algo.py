c1b1968fc07b2813e74614284e5fa626f8b79354
digimonke <23C15009@student.hcmus.edu.vn>
2025-11-01 15:55:55 +0700

    remove torch

--- demo/algo/algo.py
+++ demo/algo/algo.py
@@ -1,7 +1,5 @@
 import numpy as np
 import pandas as pd
-import torch
-from dagma.nonlinear import DagmaMLP, DagmaNonlinear
 import lingam
 from collections import defaultdict
 from typing import Callable, List, Sequence, Tuple, Optional
@@ -25,12 +23,12 @@ def linear_causal_discovery(data: pd.DataFrame) -> np.ndarray:
     # lingam uses (row i, col j) = j -> i; transpose to get i -> j
     return model.adjacency_matrix_.T
 
-def nonlinear_causal_discovery(data: pd.DataFrame) -> np.ndarray:
-    eq_model = DagmaMLP(dims=[data.shape[1], 10, 1], bias=True, dtype=torch.double) # create the model for the structural equations, in this case MLPs
-    model = DagmaNonlinear(eq_model, dtype=torch.double) # create the model for DAG learning
-    W_est = model.fit(data.values, lambda1=0.02, lambda2=0.005) # fit the model with L1 reg. (coeff. 0.02) and L2 reg. (coeff. 0.005)
+# def nonlinear_causal_discovery(data: pd.DataFrame) -> np.ndarray:
+#     eq_model = DagmaMLP(dims=[data.shape[1], 10, 1], bias=True, dtype=torch.double) # create the model for the structural equations, in this case MLPs
+#     model = DagmaNonlinear(eq_model, dtype=torch.double) # create the model for DAG learning
+#     W_est = model.fit(data.values, lambda1=0.02, lambda2=0.005) # fit the model with L1 reg. (coeff. 0.02) and L2 reg. (coeff. 0.005)
 
-    return W_est # return the estimated weighted adjacency matrix
+#     return W_est # return the estimated weighted adjacency matrix
 
 
 def stability_subsampling(
